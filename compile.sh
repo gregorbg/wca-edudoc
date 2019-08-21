bde6f74105b92f3e55ea50a9f66b441f4c4e3db6
Gregor Billing <suushiemaniac@gmail.com>
2019-08-21 23:00:00 +0200

    Add latex template to compiler toolchain

--- compile.sh
+++ compile.sh
@@ -8,7 +8,11 @@ if [ $# -ne 1 ]; then
 fi
 
 BUILD_DIR="$1/build"
-mkdir -p "$BUILD_DIR"
+LATEX_DIR="$BUILD_DIR/latex"
+mkdir -p "$LATEX_DIR"
+
+TEX_TEMPLATE="template.tex"
+TEX_PLACEHOLDER="PDF_INTERMEDIATE"
 
 # Find all Markdown files and build PDFs out of them.
 find "$1" -name '*.md' | while read file; do
@@ -17,9 +21,15 @@ find "$1" -name '*.md' | while read file; do
   FILE_BASENAME=$(basename "$file")
   FILE_BASENAME=${FILE_BASENAME%.md}
 
-  PDF_FILE="$BUILD_DIR/$FILE_BASENAME.pdf"
+  PDF_INTERMEDIATE_FILE="$BUILD_DIR/${FILE_BASENAME}_markdown.pdf"
   HTML_FILE="$BUILD_DIR/$FILE_BASENAME.html"
 
   pandoc -s --from markdown --to html5 --metadata pagetitle="$1" "$file" -o "$HTML_FILE" # Markdown -> HTML
-  wkhtmltopdf --encoding 'utf-8' --user-style-sheet "$1/style.css" -T 15mm -B 15mm -R 15mm -L 15mm --quiet "$HTML_FILE" "$PDF_FILE" # HTML -> PDF
+  wkhtmltopdf --encoding 'utf-8' -T 15mm -B 15mm -R 15mm -L 15mm --quiet "$HTML_FILE" "$PDF_INTERMEDIATE_FILE" # HTML -> PDF
+
+  TEX_SOURCE="$BUILD_DIR/$FILE_BASENAME.tex"
+
+  sed -r "s#$TEX_PLACEHOLDER#$PDF_INTERMEDIATE_FILE#g" "$TEX_TEMPLATE" > "$TEX_SOURCE"
+  lualatex --output-directory="$LATEX_DIR" "$TEX_SOURCE" > /dev/null
+  mv "$LATEX_DIR/$FILE_BASENAME.pdf" "$BUILD_DIR/$FILE_BASENAME.pdf"
 done
