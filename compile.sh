6c6869ca02cf6731271df178b2cce2f05fa635c1
Gregor Billing <gregor.billing@freiheit.com>
2019-08-22 11:39:05 +0200

    More consistent handling of input strings in compile script

--- compile.sh
+++ compile.sh
@@ -7,7 +7,9 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-BUILD_DIR="$1/build"
+PROJECT_DIR=$(basename "$1")
+
+BUILD_DIR="$PROJECT_DIR/build"
 LATEX_DIR="$BUILD_DIR/latex"
 mkdir -p "$LATEX_DIR"
 
@@ -15,7 +17,7 @@ TEX_TEMPLATE="template.tex"
 TEX_PLACEHOLDER="PDF_INTERMEDIATE"
 
 # Find all Markdown files and build PDFs out of them.
-find "$1" -name '*.md' | while read file; do
+find "$PROJECT_DIR/" -name '*.md' | while read file; do
   echo "Processing $file…"
 
   FILE_BASENAME=$(basename "$file")
@@ -24,12 +26,12 @@ find "$1" -name '*.md' | while read file; do
   PDF_INTERMEDIATE_FILE="$BUILD_DIR/${FILE_BASENAME}_markdown.pdf"
   HTML_FILE="$BUILD_DIR/$FILE_BASENAME.html"
 
-  pandoc -s --from markdown --to html5 --metadata pagetitle="$1" "$file" -o "$HTML_FILE" # Markdown -> HTML
+  pandoc -s --from markdown --to html5 --metadata pagetitle="$PROJECT_DIR" "$file" -o "$HTML_FILE" # Markdown -> HTML
   wkhtmltopdf --encoding 'utf-8' --user-style-sheet "style.css" -T 15mm -B 15mm -R 15mm -L 15mm --quiet "$HTML_FILE" "$PDF_INTERMEDIATE_FILE" # HTML -> PDF
 
   TEX_SOURCE="$BUILD_DIR/$FILE_BASENAME.tex"
 
   sed -r "s#$TEX_PLACEHOLDER#$PDF_INTERMEDIATE_FILE#g" "$TEX_TEMPLATE" > "$TEX_SOURCE"
-  lualatex --output-directory="$LATEX_DIR" "$TEX_SOURCE" > /dev/null
+  lualatex --output-directory="$LATEX_DIR" "$TEX_SOURCE"
   mv "$LATEX_DIR/$FILE_BASENAME.pdf" "$BUILD_DIR/$FILE_BASENAME.pdf"
 done
