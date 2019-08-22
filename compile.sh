8b3275b9a267361299054b250186a331f21fcfdf
Gregor Billing <gregor.billing@freiheit.com>
2019-08-22 11:40:51 +0200

    Introduce assets folder

--- compile.sh
+++ compile.sh
@@ -13,7 +13,9 @@ BUILD_DIR="$PROJECT_DIR/build"
 LATEX_DIR="$BUILD_DIR/latex"
 mkdir -p "$LATEX_DIR"
 
-TEX_TEMPLATE="template.tex"
+CSS_STYLESHEET="assets/style.css"
+
+TEX_TEMPLATE="assets/template.tex"
 TEX_PLACEHOLDER="PDF_INTERMEDIATE"
 
 # Find all Markdown files and build PDFs out of them.
@@ -27,7 +29,7 @@ find "$PROJECT_DIR/" -name '*.md' | while read file; do
   HTML_FILE="$BUILD_DIR/$FILE_BASENAME.html"
 
   pandoc -s --from markdown --to html5 --metadata pagetitle="$PROJECT_DIR" "$file" -o "$HTML_FILE" # Markdown -> HTML
-  wkhtmltopdf --encoding 'utf-8' --user-style-sheet "style.css" -T 15mm -B 15mm -R 15mm -L 15mm --quiet "$HTML_FILE" "$PDF_INTERMEDIATE_FILE" # HTML -> PDF
+  wkhtmltopdf --encoding 'utf-8' --user-style-sheet "$CSS_STYLESHEET" -T 15mm -B 15mm -R 15mm -L 15mm --quiet "$HTML_FILE" "$PDF_INTERMEDIATE_FILE" # HTML -> PDF
 
   TEX_SOURCE="$BUILD_DIR/$FILE_BASENAME.tex"
 
