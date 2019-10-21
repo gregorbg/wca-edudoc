f14db2e2e2907a2403a5990524000fbbefd7044b
Leon Schmidtchen <leon.schmidtchen@gmail.com>
2019-10-21 21:38:56 +0200

    Starting working on removing the lualatex dependency. PDF output might need some tweaks now.
    How do we handle the document name and version dynamically changing
    for the header depending on which document is compiled.
    We would probably need to store that information in the .md somewhere,
    but in what format and how do we get it in the header.html?

--- compile.sh
+++ compile.sh
@@ -9,41 +9,26 @@ fi
 
 PROJECT_DIR=$(basename "$1")
 BUILD_DIR="$PROJECT_DIR/build"
-LATEX_DIR="$BUILD_DIR/latex"
 
-mkdir -p "$LATEX_DIR"
+mkdir -p "$BUILD_DIR"
 
 ASSETS_ABS_DIR=$(pwd)/assets
 ASSETS_REL_DIR_TAG="../assets"
 
 CSS_STYLESHEET="$ASSETS_ABS_DIR/style.css"
-TEX_TEMPLATE="$ASSETS_ABS_DIR/template.tex"
-
-TEX_MD_PLACEHOLDER="PDF_INTERMEDIATE"
-TEX_TITLE_PLACEHOLDER="DOCUMENT_TITLE"
+HEADER_HTML_FILE="$ASSETS_ABS_DIR/header.html"
+FOOTER_HTML_FILE="$ASSETS_ABS_DIR/footer.html"
 
 # Find all Markdown files and build PDFs out of them.
-find "$PROJECT_DIR/" -name '*.md' | while read file; do
-  echo "Processing $file…"
+find "$PROJECT_DIR" -name '*.md' | while read file; do
+  echo "Processing $file..."
 
   FILE_BASENAME=$(basename "$file")
   FILE_BASENAME=${FILE_BASENAME%.md}
 
-  TITLE_LINE=$(head -n 1 "$file")
-  DOCUMENT_TITLE=$(echo "$TITLE_LINE" | sed -r "s/#+\s*//")
-
-  PDF_INTERMEDIATE_FILE="$BUILD_DIR/${FILE_BASENAME}_markdown.pdf"
+  PDF_FILE="$BUILD_DIR/${FILE_BASENAME}.pdf"
   HTML_FILE="$BUILD_DIR/$FILE_BASENAME.html"
 
   pandoc -s --from markdown --to html5 --metadata pagetitle="$PROJECT_DIR" "$file" -o "$HTML_FILE" # Markdown -> HTML
-  wkhtmltopdf --encoding 'utf-8' --user-style-sheet "$CSS_STYLESHEET" -T 15mm -B 15mm -R 15mm -L 15mm --quiet "$HTML_FILE" "$PDF_INTERMEDIATE_FILE" # HTML -> PDF
-
-  TEX_SOURCE="$BUILD_DIR/$FILE_BASENAME.tex"
-
-  sed -r "s#$TEX_MD_PLACEHOLDER#$PDF_INTERMEDIATE_FILE#g" "$TEX_TEMPLATE" > "$TEX_SOURCE"
-  sed -ir "s#$ASSETS_REL_DIR_TAG#$ASSETS_ABS_DIR#g" "$TEX_SOURCE"
-  sed -ir "s#$TEX_TITLE_PLACEHOLDER#$DOCUMENT_TITLE#g" "$TEX_SOURCE"
-  rm -f "${TEX_SOURCE}r"
-
-  lualatex --output-directory="$LATEX_DIR" "$TEX_SOURCE" && mv "$LATEX_DIR/$FILE_BASENAME.pdf" "$BUILD_DIR/$FILE_BASENAME.pdf"
+  wkhtmltopdf --encoding 'utf-8' --user-style-sheet "$CSS_STYLESHEET" -T 15mm -B 15mm -R 15mm -L 15mm --header-html "$HEADER_HTML_FILE" --footer-html "$FOOTER_HTML_FILE" --quiet "$HTML_FILE" "$PDF_FILE" # HTML -> PDF
 done
